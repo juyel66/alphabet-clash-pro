946ec2499402552e9cde5884c544ba2a8638b757
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-02-14 22:38:51 +0600

    some section added

--- script/different_way.js
+++ script/different_way.js
@@ -37,13 +37,73 @@ function getRandomAlphabet(){
 
 
 
+function handleKeyboardKeyupEvent(event){
+    const playerPress = event.key
+    console.log('Player Press:', playerPress);
+
+    // get the excepted to press 
+    const currentAlphabetElementById = document.getElementById('current-alphabet');
+   
+    const currentAlphabet = currentAlphabetElementById.innerText;
+    const exceptedAlphabet = currentAlphabet.toLocaleLowerCase();
+     console.log(exceptedAlphabet,playerPress);
+
+
+    //  checked match or not 
+    if(playerPress===exceptedAlphabet){
+        console.log('You get a point')
+        console.log('you have pressed correctly:', exceptedAlphabet);
+        // update score 
+        // step-1: get the current score 
+
+
+
+        
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        console.log(currentScore);
+        // step-2: increase the score by 1
+        const newScore = currentScore+1;
+        
+        // step-3: show the update score 
+        currentScoreElement.innerText=newScore;
+
+        // start a new round 
+        removeBackgroundColorById(exceptedAlphabet);
+        continueGame();
+
+    }
+    else{
+        console.log('you missed. you lost a life');
+        //step-1: get the current life number 
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // step-2: Reduce the life count 
+        const newLife = currentLife-1;
+        if(newLife===0){
+            gameOver()
+        }
+
+
+        // step-3: Display the update life count 
+        currentLifeElement.innerText= newLife;
+    }
+}
+// capture keyboard key press 
+document.addEventListener('keyup', handleKeyboardKeyupEvent)
+
+
+
 
 
 function continueGame(){
     //step-1: generate a random alphabet
     const alphabet = getRandomAlphabet();
     // console.log(alphabet);
-    console.log('Your random alphabet:',alphabet)
+    // console.log('Your random alphabet:',alphabet)
 
     // set randomly generate  alphabet to the display
     const currentAlphabetElementById = document.getElementById('current-alphabet');
@@ -60,10 +120,24 @@ function removeBackgroundColorById(elementId){
 
 
 function Play(){
+    // hide everything show only play ground 
     hiddenElementById('home-screen')
-    showElementById('play-ground')
+    hiddenElementById('final-score')
+     showElementById('play-ground')
+
+    // reset score and life 
+    
     continueGame()
     
+}
+
+// game over
+
+function gameOver(){
+    hiddenElementById('play-ground')
+    
+    showElementById('final-score')
+
 }
 
 
